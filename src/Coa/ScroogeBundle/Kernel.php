2a3ecb403962abf4a1c0e5f000800aa660cbca87
datagutten <datagutten@datagutten.net>
2023-05-14 21:34:53 +0200

    New config and kernel

---
+++ src/Coa/ScroogeBundle/Kernel.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Coa\ScroogeBundle;
+
+use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
+use Symfony\Component\HttpKernel\Kernel as BaseKernel;
+
+class Kernel extends BaseKernel
+{
+    use MicroKernelTrait;
+}
